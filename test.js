b244a0feefbc5aadaee186191b1d338e75dfaec1
Ben Drucker <bvdrucker@gmail.com>
2015-09-01 21:33:14 -0700

    Fix tests using proxyquire since min-document doesn't support el.contains

--- test.js
+++ test.js
@@ -4,10 +4,22 @@ var test = require('tape')
 var h = require('virtual-dom/h')
 var createElement = require('virtual-dom/create-element')
 var document = require('global/document')
-var Append = require('./')
+var proxyquire = require('proxyquire')
 
 test(function (t) {
   t.plan(3)
+
+  var Append = proxyquire('./', {
+    'global/document': {
+      body: {
+        contains: function contains () {
+          // element does not yet exist in body
+          // it's being appended
+          return false
+        }
+      }
+    }
+  })
 
   var vtree = h('div', {hook: Append(callback)}, 'Hello')
   var rootNode = createElement(vtree)
@@ -17,5 +29,25 @@ test(function (t) {
     t.ok(node)
     t.ok(node.parentNode)
     t.equal(node.childNodes[0].data, 'Hello')
+    document.body.removeChild(rootNode)
   }
+})
+
+test(function (t) {
+  var Append = proxyquire('./', {
+    'global/document': {
+      body: {
+        contains: function contains () {
+          // element does exist in body
+          // it's not being appended
+          return true
+        }
+      }
+    }
+  })
+
+  var vtree = h('div', {hook: Append(t.fail.bind(t, 'hook should not run'))}, 'Hello')
+  var rootNode = createElement(vtree)
+  document.body.appendChild(rootNode)
+  t.end()
 })
